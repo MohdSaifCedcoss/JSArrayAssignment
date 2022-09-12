09c5b20f16376080d3df1bf93381032e9323a583
MohdSaifCedcoss <mohdsaif2022aug@cedcoss.com>
2022-09-12 13:48:18 +0530

    Made some changes in task1 and task2

--- src/task1.js
+++ src/task1.js
@@ -1,68 +1,48 @@
-var samsung = {
-  company: "Sasmung",
-  model: "Galaxy",
-  memory: 64,
-  price: 15000,
-};
-var nokia = {
-  company: "Nokia",
-  model: "S730",
-  memory: 128,
-  price: 12000,
-};
-var xiaomi = {
-  company: "Xiaomi",
-  model: "Note",
-  memory: 32,
-  price: 12000,
-};
-var moto = {
-  company: "Motorola",
-  model: "G10",
-  memory: 32,
-  price: 15000,
-};
-var apple = {
-  company: "Apple",
-  model: "S12",
-  memory: 64,
-  price: 25000,
-};
-function addtoArray(obj) {
-  var ar = [];
-  for (var x in obj) {
-    if (obj.hasOwnProperty(x)) ar.push(obj[x]);
-  }
-  return ar;
-}
-samsungArray = addtoArray(samsung);
-nokArray = addtoArray(nokia);
-xiaArray = addtoArray(xiaomi);
-motArray = addtoArray(moto);
-appArray = addtoArray(apple);
-
+const mobile = [
+  {
+    company: "Samsung",
+    model: "Galaxy",
+    memory: 64,
+    price: 15000,
+  },
+  {
+    company: "Nokia",
+    model: "S730",
+    memory: 128,
+    price: 22000,
+  },
+  {
+    company: "Xiaomi",
+    model: "Note",
+    memory: 32,
+    price: 12000,
+  },
+  {
+    company: "Motorola",
+    model: "G10",
+    memory: 32,
+    price: 15000,
+  },
+  {
+    company: "Apple",
+    model: "S12",
+    memory: 64,
+    price: 25000,
+  },
+];
 let table =
   "<table><tr><th>Company</th><th>Model</th><th>Memory(GB)</th><th>Price</th></tr>";
-
-function pushToArray(array){
-    table +=
+mobile.forEach((e) => {
+  table +=
     "<tr><td>" +
-    array[0] +
+    e.company +
     "<td>" +
-    array[1] +
+    e.model +
     "</td><td>" +
-    array[2] +
+    e.memory +
     "</td><td>" +
-    array[3] +
-    "</td>" +
-    array[4] +
-    "</td>" +
-    "</tr>";
-}
-pushToArray(samsungArray);
-pushToArray(nokArray);
-pushToArray(xiaArray);
-pushToArray(motArray);
-pushToArray(appArray);
-table+="</table>";
-document.getElementById("showTable").innerHTML=table;
+    e.price +
+    "</td><td>";
+});
+table += "</table>";
+document.getElementById("show").innerHTML = table;
--- src/task2.js
+++ src/task2.js
@@ -1,94 +1,127 @@
-function search() {
-  var samsung = {
-    company: "Sasmung",
+const mobile = [
+  {
+    company: "Samsung",
     model: "Galaxy",
     memory: 64,
     price: 15000,
-  };
-  var nokia = {
+  },
+  {
     company: "Nokia",
     model: "S730",
     memory: 128,
-    price: 12000,
-  };
-  var xiaomi = {
+    price: 22000,
+  },
+  {
     company: "Xiaomi",
     model: "Note",
     memory: 32,
     price: 12000,
-  };
-  var moto = {
+  },
+  {
     company: "Motorola",
     model: "G10",
     memory: 32,
     price: 15000,
-  };
-  var apple = {
+  },
+  {
     company: "Apple",
     model: "S12",
     memory: 64,
     price: 25000,
-  };
+  },
+];
+let table =
+  "<table><tr><th>Company</th><th>Model</th><th>Memory(GB)</th><th>Price</th></tr>";
+mobile.forEach((e) => {
+  table +=
+    "<tr><td>" +
+    e.company +
+    "<td>" +
+    e.model +
+    "</td><td>" +
+    e.memory +
+    "</td><td>" +
+    e.price +
+    "</td><td>";
+});
+table += "</table>";
+document.getElementById("show").innerHTML = table;
+function search() {
+  let table =
+    "<table><tr><th>Company</th><th>Model</th><th>Memory(GB)</th><th>Price</th></tr>";
+  let field = document.getElementById("field").value;
   let key = document.getElementById("toSearch").value;
+  console.log(field+" "+key);
   let text;
-  key=key.toLowerCase();
-  if (key == "apple") {
-    text =
-      "Company\b" +
-      apple.company +
-      "<br>Model\b\b" +
-      apple.model +
-      "<br>Memory\b\b" +
-      apple.memory +
-      "<br>Price\b\b" +
-      apple.price;
+  key = key.toLowerCase();
+
+  value=false;
+  mobile.forEach((e) => {
+    if (field == "Company") {
+      if (e.company.toLowerCase() == key) {
+        table +=
+          "<tr><td>" +
+          e.company +
+          "<td>" +
+          e.model +
+          "</td><td>" +
+          e.memory +
+          "</td><td>" +
+          e.price +
+          "</td><td>";
+          value=true;
+      }
+    }
+    else if (field == "Model") {
+      if (e.model.toLowerCase() == key) {
+        table +=
+          "<tr><td>" +
+          e.company +
+          "<td>" +
+          e.model +
+          "</td><td>" +
+          e.memory +
+          "</td><td>" +
+          e.price +
+          "</td><td>";
+          value=true;
+      }
+    }
+    if (field == "Memory") {
+      if (e.memory == key) {
+        table +=
+          "<tr><td>" +
+          e.company +
+          "<td>" +
+          e.model +
+          "</td><td>" +
+          e.memory +
+          "</td><td>" +
+          e.price +
+          "</td><td>";
+          value=true;
+      }
+    }
+    if (field == "Price") {
+      if (e.price == key) {
+        table +=
+          "<tr><td>" +
+          e.company +
+          "<td>" +
+          e.model +
+          "</td><td>" +
+          e.memory +
+          "</td><td>" +
+          e.price +
+          "</td><td>";
+          value=true;
+      }
+    }
+  });
+  table+="</table>"
+  if(value==false){
+    document.getElementById("showRes").innerHTML = "Your item doesnt match with any items in the list";
+    return;
   }
-  else if(key=="samsung"){
-    text =
-      "Company\b" +
-      samsung.company +
-      "<br>Model\b\b" +
-      samsung.model +
-      "<br>Memory\b\b" +
-      samsung.memory +
-      "<br>Price\b\b" +
-      samsung.price;
-  }
-  else if(key=="xiaomi"){
-    text =
-      "Company\b" +
-      xiaomi.company +
-      "<br>Model\b\b" +
-      xiaomi.model +
-      "<br>Memory\b\b" +
-      xiaomi.memory +
-      "<br>Price\b\b" +
-      xiaomi.price;
-  }
-  else if(key=="motorola"){
-    text =
-    "Company\b" +
-    moto.company +
-    "<br>Model\b\b" +
-    moto.model +
-    "<br>Memory\b\b" +
-    moto.memory +
-    "<br>Price\b\b" +
-    moto.price;
-  }
-  else if(key=="nokia"){
-    text =
-    "Company\b" +
-    nokia.company +
-    "<br>Model\b\b" +
-    nokia.model +
-    "<br>Memory\b\b" +
-    nokia.memory +
-    "<br>Price\b\b" +
-    nokia.price;
-  }
-  else{
-    text="Entered Product is Not availabe or please re-check!"
-  }
-  document.getElementById("show").innerHTML = text;
+  document.getElementById("showRes").innerHTML = table;
 }
